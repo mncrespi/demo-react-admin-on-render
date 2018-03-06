6ea75047f5d9d3fe88d1e20818c93785636ee9f5
Matias Crespi <mat.cres@gmail.com>
2018-03-06 14:35:06 -0300

    CRUD Posts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
-import { PostList } from './posts';
+import { jsonServerRestClient, Admin, Resource, Delete } from 'admin-on-rest';
+import { PostList, PostCreate, PostEdit } from './posts';
 import { UserList } from './users';
 
 class App extends Component {
   render() {
     return (
       <Admin restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
-        <Resource name="posts" list={PostList} />
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} remove={Delete}/>
         <Resource name="users" list={UserList} />
       </Admin>
     )
--- src/posts.js
+++ src/posts.js
@@ -1,15 +1,60 @@
 import React from 'react';
-import { List, Datagrid, TextField, ReferenceField  } from 'admin-on-rest';
+import {
+  Create,
+  Datagrid,
+  DisabledInput,
+  Edit,
+  EditButton,
+  List,
+  LongTextInput,
+  ReferenceField,
+  ReferenceInput,
+  required,
+  SelectInput,
+  SimpleForm,
+  TextField,
+  TextInput
+} from 'admin-on-rest';
 
 export const PostList = (props) => (
   <List {...props}>
     <Datagrid>
-      <TextField source="id" />
+      <TextField source="id"/>
       <ReferenceField label="User" source="userId" reference="users">
-        <TextField source="name" />
+        <TextField source="name"/>
       </ReferenceField>
-      <TextField source="title" />
-      <TextField source="body" />
+      <TextField source="title"/>
+      <TextField source="body"/>
+      <EditButton/>
     </Datagrid>
   </List>
+);
+
+const PostTitle = ({record}) => {
+  return <span>Post {record ? `"${record.title}"` : ''}</span>;
+};
+
+export const PostEdit = (props) => (
+  <Edit title={<PostTitle/>} {...props}>
+    <SimpleForm>
+      <DisabledInput source="id"/>
+      <ReferenceInput label="User" source="userId" reference="users" validate={required}>
+        <SelectInput optionText="name"/>
+      </ReferenceInput>
+      <TextInput source="title"/>
+      <LongTextInput source="body"/>
+    </SimpleForm>
+  </Edit>
+);
+
+export const PostCreate = (props) => (
+  <Create {...props}>
+    <SimpleForm>
+      <ReferenceInput label="User" source="userId" reference="users" validate={required}>
+        <SelectInput optionText="name"/>
+      </ReferenceInput>
+      <TextInput source="title"/>
+      <LongTextInput source="body"/>
+    </SimpleForm>
+  </Create>
 );
