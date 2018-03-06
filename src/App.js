54b3cc5ed6660caec1265d74156b7459c6989f2b
Matias Crespi <mat.cres@gmail.com>
2018-03-06 14:26:40 -0300

    add users list

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
 import { PostList } from './posts';
+import { UserList } from './users';
 
 class App extends Component {
   render() {
     return (
       <Admin restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
         <Resource name="posts" list={PostList} />
+        <Resource name="users" list={UserList} />
       </Admin>
     )
   }
