502f1a2e6507a3f25882c827f25881e4a12ab480
Matias Crespi <mat.cres@gmail.com>
2018-03-06 16:46:21 -0300

    custom dashboard

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import { PostList, PostCreate, PostEdit } from './posts';
 import { UserList } from './users';
 import PostIcon from 'material-ui/svg-icons/action/book';
 import UserIcon from 'material-ui/svg-icons/social/group';
+import Dashboard from './dashboard'
 
 
 class App extends Component {
   render() {
     return (
-      <Admin restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
+      <Admin dashboard={Dashboard} restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
         <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} remove={Delete} icon={PostIcon}/>
         <Resource name="users" list={UserList} icon={UserIcon}/>
       </Admin>
