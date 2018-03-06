14bbdc1c19837ee385fc136132f3b4090c149c5b
Matias Crespi <mat.cres@gmail.com>
2018-03-06 16:55:27 -0300

    add authClient

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import { UserList } from './users';
 import PostIcon from 'material-ui/svg-icons/action/book';
 import UserIcon from 'material-ui/svg-icons/social/group';
 import Dashboard from './dashboard'
-
+import authClient from './authClient'
 
 class App extends Component {
   render() {
     return (
-      <Admin dashboard={Dashboard} restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
+      <Admin authClient={authClient} dashboard={Dashboard} restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
         <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} remove={Delete} icon={PostIcon}/>
         <Resource name="users" list={UserList} icon={UserIcon}/>
       </Admin>
