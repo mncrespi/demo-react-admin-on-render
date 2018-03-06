8528884713a54fd007fe696a0ed2fc2d122f818f
Matias Crespi <mat.cres@gmail.com>
2018-03-06 16:37:44 -0300

    set menu icons

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import { jsonServerRestClient, Admin, Resource, Delete } from 'admin-on-rest';
 import { PostList, PostCreate, PostEdit } from './posts';
 import { UserList } from './users';
+import PostIcon from 'material-ui/svg-icons/action/book';
+import UserIcon from 'material-ui/svg-icons/social/group';
+
 
 class App extends Component {
   render() {
     return (
       <Admin restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
-        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} remove={Delete}/>
-        <Resource name="users" list={UserList} />
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} remove={Delete} icon={PostIcon}/>
+        <Resource name="users" list={UserList} icon={UserIcon}/>
       </Admin>
     )
   }
